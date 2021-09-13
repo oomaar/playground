26b91283a16bb4282a7b42af722cbee08119c322
Omar <omaar_5@hotmail.com>
2021-09-13 18:27:23 +0200

    setup: Home Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./screens/Home/Home";
 
 export const App = () => {
     return (
-        <div>
-            <Home />
-        </div>
+        <Router>
+            <Switch>
+                <Route exact to="/" component={Home} />
+            </Switch>
+        </Router>
     );
 };
