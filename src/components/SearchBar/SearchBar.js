49d4aca4e7125313ba4debd426aa4384317d8852
Omar <omaar_5@hotmail.com>
2021-09-13 18:41:58 +0200

    connect Searchbar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { connect } from "react-redux";
+import { fetchVideos } from "../../redux/slices/videoSlice";
+import propTypes from "prop-types";
 import {
     Container,
 } from "./styledSearchBar";
 
-export const SearchBar = () => {
+const SearchBar = ({ fetchVideos }) => {
+    const [search, setSearch] = useState("");
+    const history = useHistory();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        history.push({
+            pathname: "/videos",
+            search: `search=${search}`
+        });
+        fetchVideos();
+        setSearch('');
+    };
+
     return (
         <Container>
-            SearchBar
+            <form onSubmit={e => handleSubmit(e)}>
+                <input
+                    type="text"
+                    value={search}
+                    required
+                    placeholder="Enter your search"
+                    onChange={e => setSearch(e.target.value)}
+                />
+            </form>
         </Container>
     );
 };
+
+SearchBar.propTypes = {
+    fetchVideos: propTypes.func.isRequired
+}
+
+export default connect(null, { fetchVideos })(SearchBar);
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { Navbar } from "./Navbar/Navbar";
 export { default as Feed } from "./Feed/Feed";
-export { SearchBar } from "./SearchBar/SearchBar";
+export { default as SearchBar } from "./SearchBar/SearchBar";
